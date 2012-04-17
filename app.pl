e4cacaf817afd93d27298193f3b44492fc0c297e
Maxim.Sviridenko <sviridenko.maxim@gmail.com>
2012-04-17 23:49:32 +0800

    add new app file

--- app.pl
+++ app.pl
@@ -4,24 +4,31 @@ use Mojo::UserAgent;
 use Mojo::DOM;
 use utf8;
 use v5.12;
+use DBIx::Custom;
 
+my $dbi=DBIx::Custom->connect(dsn=>"dbi:SQLite:dbname=db/database");
 my $ua=Mojo::UserAgent->new();
 my $tx=Mojo::DOM->new();
 my @ln=();
 
-#Get products links from subcategory, order by popularity
+
+print "Ok, let's start!\nClearing database\n";
+$dbi->delete_all(table=>'prod');
+
+say 'Get list of items...';
 $tx=$ua->get('http://video.wikimart.ru/camcoders/camcorder/?price[to]=6500&price[from]=2500&order=popularity')->res->dom;
 
 for my $l ($tx->find('.InfoModel a')->each){
 	push @ln, $l->attrs('href');
 }
-
-
-#Parse each product page
+say "Ok\nStarting parse product pages: \n\n";
+my $n=0;
 foreach my $link(@ln){
+	say "Product #$n ";
 	$tx=$ua->get($link)->res->dom;
-
 	my %prod=();
+	
+	$prod{'link'}=$link;
 
 	for my $c($tx->find('#information h1')->each){
     	$prod{'caption'}= $c->text;
@@ -43,9 +50,13 @@ foreach my $link(@ln){
 	for my $count($tx->find('.Count')->each){
     	$prod{'count'}=$count->text;
 	}
+	
 
-	foreach my $key(keys %prod){
-    	say "$key=$prod{$key}";
-	}
+	say "data recording\n";
+	$dbi->insert({link=>$prod{'link'},caption=>$prod{'caption'},image=>$prod{'image'},price=>$prod{'price'},description=>$prod{'description'},count=>$prod{'count'}},table=>'prod');
+	#foreach my $key(keys %prod){
+    #	say "$key=$prod{$key}";
+	#}
+	$n++;
 }
 
