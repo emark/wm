0632876d1c61f2fa3cb8012bc23f08e239cd7ac3
Maxim.Sviridenko <sviridenko.maxim@gmail.com>
2012-04-18 13:49:12 +0800

    add sub in app

--- app.pl
+++ app.pl
@@ -14,6 +14,9 @@ my $catfile='catalog.qr';
 my @catalog=();#catalog list
 my $catcount=0;#count of catalog categories
 
+
+#&ParseProductCard('http://video.wikimart.ru/camcoders/camcorder/model/5840562/portativnaya_mini_videokamera_kodak_playsport_zx5/');
+
 print "Ok, let's start!\nClearing database\n";
 $dbi->delete_all(table=>'prod');
 
@@ -43,33 +46,23 @@ foreach my $key(@catalog){
 	foreach my $link(@ln){
 		$n++;
 		print  "Product #$n from $allitems. Category $c from $catcount.\t";
-		$tx=$ua->max_redirects(5)->get($link=>{DNT=>1})->res->dom;
-		my %prod=();
-	
-		$prod{'link'}=$link;
-
-		for my $c($tx->find('#information h1')->each){
-    		$prod{'caption'}= $c->text;
-		}
-
-		for my $p($tx->find('.photo')->each){
-    		$prod{'image'}= $p->attrs('src');
-		}
-
-		for my $l ($tx->find('.offer-price')->each){
-    		$prod{'price'}=$l->all_text;
-	    	$prod{'price'}=~s/\s+||\р\.//g;
-		}
-
-		for my $desc($tx->find('.short-description-uncut p')->each){
-    		$prod{'description'}=$desc->text;
-		}
-
-		for my $count($tx->find('.Count')->each){
-    		$prod{'count'}=$count->text;
-		}
-	
-		say "data recording\n";
-		$dbi->insert({topcat=>$topcat,subcat=>$subcat,link=>$prod{'link'},caption=>$prod{'caption'},image=>$prod{'image'},price=>$prod{'price'},description=>$prod{'description'},count=>$prod{'count'}},table=>'prod');
+		&ParseProductCard($link);
 	}#foreach @ln
+}
+
+sub ParseProductCard(){
+	my $link=$_[0];
+	$tx=$ua->max_redirects(5)->get($link=>{DNT=>1})->res->dom;
+    my %prod=();
+
+	$prod{'link'}=$link;
+
+    for my $c($tx->find('#information h1')->each){$prod{'caption'}= $c->text;}
+    for my $p($tx->find('.photo')->each){$prod{'image'}= $p->attrs('src');}
+    for my $l ($tx->find('.offer-price')->each){$prod{'price'}=$l->all_text;$prod{'price'}=~s/\s+||\р\.//g;}
+	$prod{'description'}='';
+    for my $desc($tx->find('.description p')->each){$prod{'description'}=$prod{'description'}.$desc->text;}
+    for my $count($tx->find('.Count')->each){$prod{'count'}=$count->text;}
+    say "data recording\n";
+    $dbi->insert({topcat=>$topcat,subcat=>$subcat,link=>$prod{'link'},caption=>$prod{'caption'},image=>$prod{'image'},price=>$prod{'price'},description=>$prod{'description'},count=>$prod{'count'}},table=>'prod');
 }
